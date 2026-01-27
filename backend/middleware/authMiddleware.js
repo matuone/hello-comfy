bd67d68ebaf65bd227597b5aa1d54a8b0f5ecbeb
matiascastells <matiascastells@gmail.com>
2026-01-27 15:32:07 -0300

    me olvide guardar archivos anteriores con el fix

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,22 +2,22 @@ import jwt from "jsonwebtoken";
 
 export default function authMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
-    // console.log removido por seguridad
+  // console.log removido por seguridad
   const token = authHeader?.split(" ")[1];
 
   if (!token) {
-     // console.log removido por seguridad
+    // console.log removido por seguridad
     return res.status(401).json({ error: "Token no proporcionado" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      // console.log removido por seguridad
+    // console.log removido por seguridad
     // Guardamos los datos del usuario en req.user
     req.user = decoded;
     next();
   } catch (err) {
-      // console.log removido por seguridad
+    // console.log removido por seguridad
     return res.status(401).json({ error: "Token inválido" });
   }
 }
