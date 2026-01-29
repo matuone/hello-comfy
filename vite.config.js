3acf1eda9c6b19cd8acf0f0e354917c79f049839
matiascastells <matiascastells@gmail.com>
2026-01-29 00:40:14 -0300

    test railway 2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "./", // ⭐ CLAVE PARA QUE FUNCIONE EN RAILWAY
   plugins: [react()],
   css: {
     modules: {
